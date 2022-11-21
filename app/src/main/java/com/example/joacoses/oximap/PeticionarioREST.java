e2fd97632660e008e043d1a263590b03664eba81
Joan Costa Escrivà <joance3@gmail.com>
2022-11-21 13:24:20 +0100

    -Nombre del comit:     Mejor explicacion de los comentarios
    -Descripción del comit:
        Se ha mejorado los comentarios previos de algunas funciones

--- app/src/main/java/com/example/joacoses/oximap/PeticionarioREST.java
+++ app/src/main/java/com/example/joacoses/oximap/PeticionarioREST.java
@@ -56,7 +56,6 @@ public class PeticionarioREST extends AsyncTask<Void, Void, Boolean> {
     }
 
     // .................................................................
-    // params: Void -->
     // onPostExecute() -->
     // return bool
     // .................................................................
